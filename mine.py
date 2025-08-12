adef59b3725f912a24ebcdf984f0647e4a96eb57
Saksham <sakshamdubey12@gmail.com>
2025-08-12 10:16:38 +0530

    first commit

---
+++ mine.py
@@ -0,0 +1,12 @@
+str="HELLO"
+str1="MY name is name saksham "
+txt = "H\te\tl\t{price:.2f}\to"
+b=str.capitalize()
+c=str.casefold()
+d=str.center(20)
+e=str1.count("name")
+f=str.endswith("O")
+g=txt.expandtabs(2)
+h=str1.find("n")
+print(txt.format(price=49))
+print(h)
